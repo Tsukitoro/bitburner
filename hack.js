3f637d176e163b069aa110c7f28f15126cc4f37f
Tsukitoro <c0rel@mail.ru>
2022-09-09 02:21:26 +0900

    update loggin for hack

--- hack.js
+++ hack.js
@@ -3,16 +3,27 @@ export async function main(ns) {
 	// Loggin
 	ns.disableLog('getServerMoneyAvailable');
 	ns.disableLog('getServerSecurityLevel');
+	ns.disableLog('getServerMinSecurityLevel');
+	ns.disableLog('getServerMaxMoney');
+
+    // Config
+    const server = ns.getHostname()
+    const minSecurityLevel = ns.getServerMinSecurityLevel(server)
+    const maxMoney = ns.getServerMaxMoney(server)
+
+    ns.print(`
+        Server name: ${server}
+        Server minimum security level: ${minSecurityLevel}
+        Server max money: ${maxMoney}
+    `)
 
     // Run endless hack
-    const server = ns.getHostname()
-	
 	while (true) {
-		if (ns.getServerSecurityLevel(server) > ns.getServerMinSecurityLevel(server)) {
+		if (ns.getServerSecurityLevel(server) > minSecurityLevel) {
 			ns.print('Weaking server...')
             await ns.weaken(server);
 			ns.print(`ServerSecurityLevel: ${ns.getServerSecurityLevel(server)}`)
-		} else if (ns.getServerMoneyAvailable(server) < ns.getServerMaxMoney(server)) {
+		} else if (ns.getServerMoneyAvailable(server) < maxMoney) {
 			ns.print('Adding currency...')
 			await ns.grow(server);
 			ns.print(`Server currency: ${ns.getServerMoneyAvailable(server)}`)
